cbd391d559e3928ab65f14fb2851423aaf45b762
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-05-17 03:29:30 +0200

    Fix fixture paths

--- spec/pdqhash_spec.rb
+++ spec/pdqhash_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Pdqhash do
   end
 
   it "does something useful" do
-    filepath = Bundler.root.join('spec', 'fixtures', 'files', 'bridge-1-original.jpg')
+    filepath = Bundler.root.join('spec', 'fixtures', 'files', 'bridge-1-original.jpg').to_s
     puts filepath
     expect(Pdqhash.hash(filepath)).to eq("f8f8f0cee0f4a84f06370a22038f63f0b36e2ed596621e1d33e6b39c4e9c9b22")
   end
