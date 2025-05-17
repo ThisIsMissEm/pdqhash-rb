33b8d438a80596f9d809657df01173c69031dbce
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-05-17 03:21:46 +0200

    Try to add a test case

--- spec/pdqhash_spec.rb
+++ spec/pdqhash_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Pdqhash do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(Pdqhash.hash(file_fixture('bridge-1-original.jpg'))).to eq("f8f8f0cee0f4a84f06370a22038f63f0b36e2ed596621e1d33e6b39c4e9c9b22")
   end
 end
